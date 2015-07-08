0b4a99961c7396ce3004186ec78fe53acafe24a8
Adi <tsaitungtso@gmail.com>
2015-07-08 18:11:42 +0800

    hw2_1

--- adiCV/homework/hw2_1.cpp
+++ adiCV/homework/hw2_1.cpp
@@ -7,15 +7,22 @@ using namespace std;
 void swap(char *tmp)
 {
 	//cout << "tmp = " << tmp << endl;
-	tmp = "cba";
+	//printf("*tmp >>> %p\n", tmp);
+	tmp[0]='c';
+	tmp[2]='a';	
+	//tmp = "cba"; //錯誤, 右邊abc電腦編譯新的位置給tmp,並非改變tmp原本指的位置的字串
 	//cout << "tmp = " << tmp << endl;
+	//printf("%p\n", tmp);
 	return;
 }
 
 int main(void)
 {
-	char *str="abc";
+	char str[16];
+
+	strcpy(str, "abc");
 	//size_t len;
+	printf("%c\n", *str);// 印出str所指的內容（a）
 	printf("%p\n", str);// 印出str所存的位置（字串a的位置）
 	cout << "before swap str = " << str << endl;// cout 編譯會直接讀取位置上的內容值
 	//cout << "str = " << *str << endl;// 印出str所存的位置的內容（第一個位置的字串a）
