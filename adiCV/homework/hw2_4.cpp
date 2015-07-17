6130ef0a8db4b7f24ad2de327831dc4e2bee2251
Adi <tsaitungtso@gmail.com>
2015-07-17 19:20:12 +0800

    HW

--- adiCV/homework/hw2_4.cpp
+++ adiCV/homework/hw2_4.cpp
@@ -4,27 +4,22 @@
 #include <string.h>
 using namespace std;
 
-void swap(string* tmp1)
+void swap(string& tmp1)
 {
-
-	//cout << "lenght of str is: " << (*tmp1).size() << endl;
-
 	int i;
-    char* char_str1;
-	char_str1 = new char[tmp1->size()];	// [tmp1->size()] is equal to [(*tmp1).size()];
-	
-	strcpy(char_str1, tmp1->c_str());
-	
+	char* char_str1;
+	char_str1 = new char[tmp1.size()];
 	char* char_str2;
-	char_str2 = new char[tmp1->size()];
+	char_str2 = new char[tmp1.size()];
+	strcpy(char_str1, tmp1.c_str());
 
-	for(i=0;i<tmp1->size();i++)
+	for(i=0;i<tmp1.size();i++)
 	{
-		char_str2[i] = char_str1[tmp1->size()-i-1];
+		char_str2[i] = char_str1[tmp1.size()-i-1];
 	}
 	cout << "in swap, the string of 'char_str' is " <<  char_str2  << endl;
-	*tmp1 = char_str2;	
-
+	tmp1 = char_str2;
+	cout << "in swap, the string of 'tmp1' is " <<  tmp1  << endl;
 	delete [] char_str1;
 	delete [] char_str2;
 	return;
@@ -35,9 +30,9 @@ int main(void)
 	std::string s;
 	cout << "please enter a string : ";
 	std::getline(std::cin, s);
-
+	
 	cout << "before swap, the string is " << s << endl;
-	swap(&s);
+	swap(s);
 	cout << "after swap, the string is  " << s << endl;
 	return 0;
 }
